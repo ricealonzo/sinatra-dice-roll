de3f643e53d4cfb350a8a45df4280649255742fe
Alonzo Rice <96135977+ricealonzo@users.noreply.github.com>
2024-04-19 15:00:53 +0000

    2

--- dice.rb
+++ dice.rb
@@ -2,33 +2,42 @@ require "sinatra"
 require "sinatra/reloader"
 
 
-get("/")do
-  "<link href="/dice/2/6" ></link>"
+get("/")do  
+"<h1> Pick and Roll </h1>
+  <a href='/zebra'>Zebra</a>
+  <a href='/dice/2/6'>2 Dice 6 Sides</a>
+  <a href='/dice/2/10'>2 Dice 10 Sides</a>
+  <a href='/dice/1/20'>1 Die 20 Sides</a>
+  <a href='/dice/5/4'>5 Dice 4 Sides</a>
+  <a href='/dice/3/12'>3 Dice 12 Sides</a>
+  "
   
 end
 
 
 get("/zebra")do
-  "We must add the route in order for the browser to connect to the path"
+"We must add the route in order for the browser to connect to the path"
 end
 
 get("/giraffe")do
-  "this is to make sure your app is reloading automatically `gem sinatra-contrib` "
+"this is to make sure your app is reloading automatically `gem sinatra-contrib` "
 end
 
 get("/dice/2/6")do
-  first_die  = rand(1..6)
-  
-  second_die  = rand(1..6)
-  
-  sum = first_die + second_die
+first_die  = rand(1..6)
 
-  outcome = "Your first die rolled #{first_die
-  } and your second die rolled #{second_die
-  } your total is #{sum}"
+second_die  = rand(1..6)
 
- " <h1>2 dice 6 Sides</h1>
-  <h3> #{outcome} </h3> "
+sum = first_die + second_die
+
+outcome = "Your first die rolled #{first_die
+} and your second die rolled #{second_die
+} your total is #{sum}"
+
+" <h1>2 dice 6 Sides</h1>
+<h3> #{outcome} </h3> 
+<a href='/dice/2/6'>Roll Again</a>
+<a href='/'>Home Page</a>"
   
 end
 
@@ -45,7 +54,9 @@ outcome = "Your first die rolled #{first_die
 } your total is #{sum}"
 
 " <h1>2 dice 10 Sides</h1>
-<h3> #{outcome} </h3> "
+<h3> #{outcome} </h3><br>
+<a href='/dice/2/6'>Roll Again</a>
+<a href='/'>Home Page</a>"
 end
 
 
@@ -54,7 +65,9 @@ get("/dice/1/20")do
 first_die = rand(1..20)
 
 "<h1>1 die 20 Sides <h1>
-<h3>You Have rolled #{first_die}!</h>
+<h3>You Have rolled #{first_die}!<h3><br>
+<a href='/dice/2/6'>Roll Again</a>
+<a href='/'>Home Page</a></h>
 "
 end
 
@@ -77,7 +90,9 @@ outcome = "Your first die rolled #{first_die
 } third die rolled #{third_die} your fourth die rolled#{fourth_die} your fifth die rolled #{fifth_die}your total is #{sum}"
 
 " <h1>5 dice 4 Sides</h1>
-<h3> #{outcome} </h3> "
+<h3> #{outcome} </h3><br>
+<a href='/dice/2/6'>Roll Again</a>
+<a href='/'>Home Page</a> "
 end
 
 
@@ -100,5 +115,8 @@ get("/dice/3/12")do
       <td>#{third_die} </td>
       <td>#{sum} </td>
     </tr>
-  </table>" 
+  </table>
+  <br>
+<a href='/dice/2/6'>Roll Again</a>
+<a href='/'>Home Page</a>" 
 end
